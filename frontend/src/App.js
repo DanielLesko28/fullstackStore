857e014f802988aca01cb8e838a07379e8046812
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-03-04 08:32:36 +0100

    saving header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
