b84f83fc53b196fad5009aa99a4334069346d0bf
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-03-04 09:02:18 +0100

    rendering products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { BrowserRouter } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./pages/HomeScreen";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <main>
-          <Container>
-            <h1>Welcome to my shop</h1>
-            <HomeScreen />
-          </Container>
-        </main>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <main>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,58 @@ import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import { Provider } from "react-redux";
+import HomeScreen from "./pages/HomeScreen";
 // import store from "./yourReduxStorePath"; // Import your Redux store
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/search/:keyword" element={<HomeScreen />} />
+      <Route path="/page/:pageNumber" element={<HomeScreen />} />
+      <Route
+        path="/search/:keyword/page/:pageNumber"
+        element={<HomeScreen />}
+      />
+      {/* <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} /> */}
+      {/* Registered users */}
+      {/* <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route> */}
+      {/* Admin users */}
+      {/* <Route path="" element={<AdminRoute />}>
+        <Route path="/admin/orderlist" element={<OrderListScreen />} />
+        <Route path="/admin/productlist" element={<ProductListScreen />} />
+        <Route
+          path="/admin/productlist/:pageNumber"
+          element={<ProductListScreen />}
+        />
+        <Route path="/admin/userlist" element={<UserListScreen />} />
+        <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+        <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+      </Route> */}
+    </Route>
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    <App />
+    <RouterProvider router={router} />
     {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
