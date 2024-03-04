7d455e486583f6fbac64ddf73f01893f7e8fea7d
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-03-04 11:49:31 +0100

    db connection and models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 
 const port = process.env.PORT || 5000;
 
