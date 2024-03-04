cb369b064f6b0d7a9d47f368877f0fcae5a3a6fd
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-03-04 10:53:36 +0100

    fetching data from backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import products from "./data/products.js";
+import dotenv from "dotenv";
 
-const port = 5000;
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
