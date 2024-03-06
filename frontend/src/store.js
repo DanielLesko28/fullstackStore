1fade10fe2137429707270ccfd8c66c860132727
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-03-06 09:09:28 +0100

    cart functionality in redux is done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
-// import cartSliceReducer from "./slices/cartSlice";
+import cartSliceReducer from "./slices/cartSlice";
 // import authReducer from "./slices/authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    // cart: cartSliceReducer,
+    cart: cartSliceReducer,
     // auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
