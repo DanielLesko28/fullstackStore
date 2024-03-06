990377169d620aec486c80df760fce0c3aa27945
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-03-06 13:51:40 +0100

    removing functionality from cart is working

--- frontend/src/pages/CartScreen.jsx
+++ frontend/src/pages/CartScreen.jsx
@@ -11,7 +11,7 @@ import {
 } from "react-bootstrap";
 import { FaTrash } from "react-icons/fa";
 import Message from "../components/Message";
-import { addToCart } from "../slices/cartSlice";
+import { addToCart, removeFromCart } from "../slices/cartSlice";
 
 const CartScreen = () => {
   const navigate = useNavigate();
@@ -26,9 +26,9 @@ const CartScreen = () => {
     dispatch(addToCart({ ...product, qty }));
   };
 
-  //   const removeFromCartHandler = (id) => {
-  //     dispatch(removeFromCart(id));
-  //   };
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
 
   const checkoutHandler = () => {
     navigate("/login?redirect=/shipping");
@@ -73,7 +73,7 @@ const CartScreen = () => {
                     <Button
                       type="button"
                       variant="light"
-                      //   onClick={() => removeFromCartHandler(item._id)}
+                      onClick={() => removeFromCartHandler(item._id)}
                     >
                       <FaTrash />
                     </Button>
