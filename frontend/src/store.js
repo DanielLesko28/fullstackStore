474babab62757b9463286835edf618c26193803d
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-03-08 08:40:58 +0100

    login functionality in redux is done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import cartSliceReducer from "./slices/cartSlice";
-// import authReducer from "./slices/authSlice";
+import authReducer from "./slices/authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
-    // auth: authReducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
